33a350af7836d993a9321c33dcab2e537b75f417
huangjuhua <huangjuhua@ppzuche.com>
2016-05-27 11:06:01 +0800

    change diretory name

---
+++ graph/src/main/java/PP/pp_graph/Node.java
@@ -0,0 +1,13 @@
+package PP.pp_graph;
+
+abstract class Node {
+
+	@Override
+	public boolean equals(Object ob) {
+		return (Boolean) null;
+	}
+	@Override
+	public int hashCode(){
+		return (Integer) null;
+	}
+}
